aa49c9cfef1594cbd7d2faea3e647b7ca26da1ae
Irish Rivera <162866325+kelleeerrrr@users.noreply.github.com>
2024-09-30 07:45:05 +0800

    Update GetArrayMean.java

--- Labaratory Activities/Labaratory Activity 2 - 30-September-2024/GetArrayMean.java
+++ Labaratory Activities/Labaratory Activity 2 - 30-September-2024/GetArrayMean.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
 
+public class GetArrayMean {
+
+    public static double getArrayMean(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return (double) sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter number: ");
+            arr[i] = scanner.nextInt();
+        }
+
+        double mean = getArrayMean(arr);
+        System.out.printf("Mean of array is: %.2f", mean);
+
+        scanner.close();
+    }
+}
