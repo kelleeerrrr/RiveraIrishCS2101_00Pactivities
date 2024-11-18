139af04c4157887266440e6d9d4220b80430ab53
Irish Rivera <162866325+kelleeerrrr@users.noreply.github.com>
2024-11-18 11:57:28 +0800

    Update Painter.java

--- Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Painter.java
+++ Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Painter.java
@@ -1,1 +1,15 @@
+package Artists;
 
+public class Painter extends Artist {
+    private String medium;
+
+    public Painter(String name, int age, String medium) {
+        super(name, age, "Visual Arts");
+        this.medium = medium;
+    }
+
+    @Override
+    public String displayDetails() {
+        return displayBaseDetails() + "\nMedium: " + medium;
+    }
+}
