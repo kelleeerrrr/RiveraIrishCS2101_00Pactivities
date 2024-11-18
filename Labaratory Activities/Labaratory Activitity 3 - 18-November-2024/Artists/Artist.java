509ec009469a9ed2d42e2f3178c537dacec40ca2
Irish Rivera <162866325+kelleeerrrr@users.noreply.github.com>
2024-11-18 11:45:21 +0800

    Update Artist.java

--- Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Artist.java
+++ Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Artist.java
@@ -1,1 +1,34 @@
 
+package Artists;
+
+public abstract class Artist {
+    private String name;
+    private int age;
+    private String specialty;
+
+    public Artist(String name, int age, String specialty) {
+        this.name = name;
+        this.age = age;
+        this.specialty = specialty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    // Method to display basic artist details
+    public String displayBaseDetails() {
+        return "Artist Name: " + name + "\nAge: " + age + "\nSpecialty: " + specialty;
+    }
+
+    // Abstract method to be overridden in the derived classes
+    public abstract String displayDetails();
+}
