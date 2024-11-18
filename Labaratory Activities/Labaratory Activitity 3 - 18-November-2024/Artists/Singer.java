12d03d4668296bd7e6ec484e3aa8bb24d9cad909
Irish Rivera <162866325+kelleeerrrr@users.noreply.github.com>
2024-11-18 11:57:57 +0800

    Update Singer.java

--- Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Singer.java
+++ Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Singer.java
@@ -1,1 +1,13 @@
+public class Singer extends Artist {
+    private String genre;
 
+    public Singer(String name, int age, String genre) {
+        super(name, age, "Music");
+        this.genre = genre;
+    }
+
+    @Override
+    public String displayDetails() {
+        return displayBaseDetails() + "\nGenre: " + genre;
+    }
+}
