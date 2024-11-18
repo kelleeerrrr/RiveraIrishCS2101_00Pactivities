a8a38b5180b2f2d3265d12811df4b6f966e32eb4
Irish Rivera <162866325+kelleeerrrr@users.noreply.github.com>
2024-11-18 11:58:27 +0800

    Update Writer.java

--- Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Writer.java
+++ Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Writer.java
@@ -1,1 +1,15 @@
+package Artists;
 
+public class Writer extends Artist {
+    private String writingStyle;
+
+    public Writer(String name, int age, String writingStyle) {
+        super(name, age, "Writing");
+        this.writingStyle = writingStyle;
+    }
+
+    @Override
+    public String displayDetails() {
+        return displayBaseDetails() + "\nWriting Style: " + writingStyle;
+    }
+}
