4fc4ad59cc9a90cc218414b14560c745f8418b8b
Irish Rivera <162866325+kelleeerrrr@users.noreply.github.com>
2024-11-18 11:57:03 +0800

    Update Dancer.java

--- Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Dancer.java
+++ Labaratory Activities/Labaratory Activitity 3 - 18-November-2024/Artists/Dancer.java
@@ -1,1 +1,15 @@
+package Artists;
 
+public class Dancer extends Artist {
+    private String danceStyle;
+
+    public Dancer(String name, int age, String danceStyle) {
+        super(name, age, "Dance");
+        this.danceStyle = danceStyle;
+    }
+
+    @Override
+    public String displayDetails() {
+        return displayBaseDetails() + "\nDance Style: " + danceStyle;
+    }
+}
